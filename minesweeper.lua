4f840ec20b662fd505882e0d6f7fdfa1783e5a4d
Kyle Kowalski <kskowa@gmail.com>
2014-04-04 12:23:36 -0400

    Initial File commit

---
+++ minesweeper.lua
@@ -0,0 +1,32 @@
+
+--[[
+a ={};
+a['is_mine'] = false;
+a['nbr_mines'] = 0;
+a['visible'] = false;
+--]]
+
+
+
+
+function get_board_size()
+--IO needed to get board size
+
+	return size
+end
+
+
+function get_percent_mine()
+--IO needed in this to get percent
+
+
+	return percent;
+end
+
+
+
+function is_main(_arg,...)
+
+
+
+end
